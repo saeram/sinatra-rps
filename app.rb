0197c6dc022b49e10ff56de96915c960ead1f141
Saeram Evensen <105816958+saeram@users.noreply.github.com>
2023-10-13 15:44:44 +0000

    RPS game using sinatra

--- app.rb
+++ app.rb
@@ -1,9 +1,42 @@
 require "sinatra"
 require "sinatra/reloader"
 
+moves = ['rock', 'paper', 'scissors']
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
+end
+
+get("/rock") do
+  @com_move = moves.sample
+  if @com_move == 'rock'
+    @outcome = 'tied'
+    elsif @com_move == 'paper'
+    @outcome = 'lost'
+    else @outcome = 'won'
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  @com_move = moves.sample
+  if @com_move == 'paper'
+    @outcome = 'tied'
+    elsif @com_move == 'scissors'
+    @outcome = 'lost'
+    else @outcome = 'won'
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  @com_move = moves.sample
+  if @com_move == 'scissors'
+    @outcome = 'tied'
+    elsif @com_move == 'rock'
+    @outcome = 'lost'
+    else @outcome = 'won'
+  end
+  erb(:scissors)
 end
